b246f3029781fe4d7e8e7817128a6a1526c5b901
MOhamed Sobhy <Sobhy6201@Gmail.com>
2020-12-19 18:51:07 +0200

    lecture 9 assignment sove

---
+++ my_packages/Operations/Math.py
@@ -0,0 +1,6 @@
+def get_average(lst):
+    sum_of_list = 0
+    for element in lst:
+        sum_of_list += element
+    return sum_of_list / len(lst)
+    
---
+++ my_packages/Transformers/StringTransformer.py
@@ -0,0 +1,7 @@
+# ALL fUNCTIONS tHAT WE wILL nEED 
+def reverse_word(word):
+    return word[::-1]
+
+
+def capitalize_word(word):
+    return word.capitalize()
